234b5bc2c42a9587f9ab4f8b80d7ac317fef0d30
Stivenjs <stevenjaime164q@gmail.com>
2025-01-15 22:38:57 -0500

    Redeploy del backend

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth, secret } from "@aws-amplify/backend";
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,31 +7,5 @@ import { defineAuth, secret } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      google: {
-        clientId: secret("GOOGLE_CLIENT_ID"),
-        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-      },
-
-      facebook: {
-        clientId: secret("FACEBOOK_CLIENT_ID"),
-        clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
-
-        attributeMapping: {
-          fullname: "name",
-          profilePicture: "photo",
-        },
-      },
-
-      callbackUrls: ["http://localhost:3000"],
-      logoutUrls: ["http://localhost:3000"],
-    },
-  },
-
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: false,
-    },
   },
 });
