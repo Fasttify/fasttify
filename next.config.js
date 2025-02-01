fb3f6ca2819db87793ca82c1c51bdb45cd409a6c
Stivenjs <stevenjaime164q@gmail.com>
2025-01-31 19:17:54 -0500

    Agregar configuración de externals para mercadopago en next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.externals = {
+        mercadopago: "mercadopago",
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
