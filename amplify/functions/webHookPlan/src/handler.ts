5ac675f62205e88d9fe170fe86bce49694b5a399
Stivenjs <stevenjaime164q@gmail.com>
2025-02-06 15:03:25 -0500

    Actualizar la ruta de importación del entorno en el manejador de webhook

--- amplify/functions/webHookPlan/src/handler.ts
+++ amplify/functions/webHookPlan/src/handler.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { getAmplifyDataClientConfig } from "@aws-amplify/backend/function/runtime";
-import { env } from "$amplify/env/HookPlan";
+import { env } from "../../../../.amplify/generated/env/hookPlan";
 import { type Schema } from "../../../data/resource";
 
 // Configurar Amplify usando la configuración para funciones con acceso a datos
