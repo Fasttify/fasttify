46c5561c3254e23735edd63da4ff53cbf0f7c5de
Stivenjs <stevenjaime164@gmail.com>
2025-05-31 12:31:32 -0500

    feat(dependencies): update Radix UI components and enhance Tailwind configuration
    This commit updates several Radix UI components in the project, including the alert dialog and button components, to their latest versions. It also modifies the Tailwind configuration to improve styling consistency and adds a new PostCSS dependency. Additionally, the global CSS file is updated for better structure, and minor adjustments are made to the image selector modal for improved functionality.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,8 +78,14 @@ module.exports = {
       },
       keyframes: {
         pulse: {
-          '0%, 100%': { opacity: '0.5', transform: 'scale(1)' },
-          '50%': { opacity: '0.8', transform: 'scale(1.05)' },
+          '0%, 100%': {
+            opacity: '0.5',
+            transform: 'scale(1)',
+          },
+          '50%': {
+            opacity: '0.8',
+            transform: 'scale(1.05)',
+          },
         },
         moveHorizontal: {
           '0%': {
