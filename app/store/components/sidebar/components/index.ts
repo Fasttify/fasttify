fb863b0da39a49e243fee98ca1de8aefa77b91e2
Stivenjs <stevenjaime164@gmail.com>
2025-06-14 17:57:26 -0500

    refactor(sidebar): remove CustomActionsBar export to clean up component index

--- app/store/components/sidebar/components/index.ts
+++ app/store/components/sidebar/components/index.ts
@@ -2,4 +2,3 @@
 export { PolarisLayout } from '@/app/store/components/sidebar/components/polaris-layout'
 export { TopBarPolaris } from '@/app/store/components/sidebar/components/top-bar-polaris'
 export { NavigationPolaris } from '@/app/store/components/sidebar/components/navigation-polaris'
-export { CustomActionsBar } from '@/app/store/components/sidebar/components/custom-actions-bar'
