61e6b7423ce86cd4f6454df004731800813a3b8a
Steven <stevenjaime164@gmail.com>
2025-08-22 22:40:35 -0500

    refactor(products): Remove the `useProducts` hook and its dependencies
    - The `useProducts.ts` file, which contained the logic for managing products, including CRUD functions and pagination, has been removed.
    - This removal simplifies the code structure and improves project maintainability by removing unnecessary dependencies.

---
+++ app/store/hooks/data/useProducts/index.ts
@@ -0,0 +1,14 @@
+// Exportar el hook principal
+export { useProducts } from './useProducts';
+
+// Exportar tipos
+export type * from './types';
+
+// Exportar utilidades
+export * from './utils';
+
+// Exportar mutaciones
+export * from './mutations';
+
+// Exportar queries
+export * from './queries';
---
+++ app/store/hooks/data/useProducts/queries/index.ts
@@ -0,0 +1,1 @@
+export { useProductQueries } from './useProductQueries';
