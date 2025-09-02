00c0b840bd2fc6bc047e2ae67bc6db413126ebb4
Steven <stevenjaime164@gmail.com>
2025-09-02 18:21:45 -0500

    Amplify configuration optimization: The SSR option has been removed from the Amplify configuration. Additionally, a feature to extract the S3 path from a full URL has been added to the useSecureUrl hook, improving the handling of secure URLs when making requests.

--- app/(main-layout)/page.tsx
+++ app/(main-layout)/page.tsx
@@ -8,9 +8,7 @@ import { Amplify } from 'aws-amplify';
 import DocsLanding from '@/app/(main-layout)/landing/components/DocsLanding';
 import outputs from '@/amplify_outputs.json';
 
-Amplify.configure(outputs, {
-  ssr: true,
-});
+Amplify.configure(outputs);
 
 const existingConfig = Amplify.getConfig();
 Amplify.configure({
