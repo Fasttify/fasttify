4d356f5739bbcbd675468603c28ad4cfba54bcd2
Steven <stevenjaime164@gmail.com>
2025-09-05 12:34:11 -0500

    Update: Improvements have been made to the NotificationPopover component, including optimized notification display logic and improved interaction when marking notifications as read. Styles have been corrected, and the accessibility of interactive elements has been improved.

---
+++ app/store/icons/index.ts
@@ -0,0 +1,12 @@
+import { Products, Inventory, Products2, Design, Domain, Name, Shipping, Payments } from '@/app/store/icons/icons';
+
+export const Icons = {
+  Products,
+  Inventory,
+  Products2,
+  Design,
+  Domain,
+  Name,
+  Shipping,
+  Payments,
+};
