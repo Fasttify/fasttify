b2cc0f5a877280067dca7d596b34ed6b3293b050
Steven <stevenjaime164@gmail.com>
2025-10-17 14:40:22 -0500

    Add Jest setup for matchMedia and ResizeObserver mocks; update StoreSelector tests with AppProvider wrapper

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,3 +5,23 @@ process.env.APP_ENV = 'test';
 process.env.DEV_CACHE_ENABLED = 'true';
 
 global.console.warn = jest.fn();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
